aae6a318fa8e52207253bfc0602550d9a6818cea
PamelaRosa <pa.rosa4@gmail.com>
2024-07-19 23:37:09 -0300

    Criação dos métodos de Post, Get e GetById dos filmes com padrão de retorno e validações iniciais

---
+++ MoviesAPI/Controllers/MoviesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Models;
+using System.Collections.Generic;
+
+namespace MoviesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MoviesController : ControllerBase
+{
+    private static List<Movie> movies = new List<Movie>();
+    private static int id = 0;
+
+    [HttpGet]
+    public IActionResult GetMovies([FromQuery] int skip = 0, [FromQuery] int take = 10)
+    {
+        IEnumerable<Movie> filteredMovies = movies.Skip(skip).Take(take);
+
+        return Ok(filteredMovies);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetMovieByID(int id)
+    {
+        Movie? movie = movies.FirstOrDefault(f => f.id == id);
+        if(movie == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(movie);
+    }
+
+    [HttpPost]
+    public IActionResult CreateMovie([FromBody] Movie movie)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        movie.id = id++;
+        movies.Add(movie);
+        
+        return CreatedAtAction(nameof(GetMovieByID), 
+            new { id = movie.id}, 
+            movie);
+    }
+}
---
+++ MoviesAPI/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.Models
+{
+    public class Movie
+    {
+        public int id { get; internal set; }
+
+        [Required(ErrorMessage = "O título é obrigatório.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "O gênero é obrigatório.")]
+        [MaxLength(50, ErrorMessage = "O tamanho máximo do gênero é de 50 caracteres.")]
+        public string Genre { get; set; }
+
+        [MaxLength(50, ErrorMessage = "O tamanho máximo do nome do(a) diretor(a) é de 50 caracteres.")]
+        public string? Director { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        [Required(ErrorMessage = "A duração é obrigatório.")]
+        [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos.")]
+        public int Duration { get; set; }
+        [MaxLength(200, ErrorMessage = "O tamanho máximo da descrição é de 200 caracteres.")]
+        public string? Description { get; set; }
+    }
+}
